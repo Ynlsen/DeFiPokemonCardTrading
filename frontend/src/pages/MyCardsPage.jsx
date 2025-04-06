ff0b5c1a48373dc83e11946b36ddfdc0b618a489
Ynlsen <yannick.niederer@shinternet.ch>
2025-04-06 20:55:54 +0200

    updated MyCards page to use new contract features

--- frontend/src/pages/MyCardsPage.jsx
+++ frontend/src/pages/MyCardsPage.jsx
@@ -121,7 +121,7 @@ const MyCardsPage = () => {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {cards.map(card => (
-            <PokemonCard tokenId={card.tokenId} />
+            <PokemonCard tokenId={card} />
           ))}
         </div>
       )}
